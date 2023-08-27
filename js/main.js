45853ea12dd3f1ffc6a18eb7b2ab6b7907a47309
Krish-Depani <krishdepani@gmail.com>
2023-08-27 18:28:58 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector('.hamburger');
+const menuBar = document.querySelector('.menu-bar');
+const menuList = document.querySelector('.nav-menu');
+
+menuBtn.addEventListener('click', showMenu);
+
+function showMenu(){
+    menuBtn.classList.toggle('is-active');
+    menuBar.classList.toggle('is-active');
+    menuList.classList.toggle('is-active');
+}
